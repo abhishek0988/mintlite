e86a712b026d336fc2d5ebf6efa3539958ac2234
Abhishek Agrawal <abhishek88@gmail.com>
2020-12-29 13:12:35 -0800

    Update JWT client

--- google/google_client.js
+++ google/google_client.js
@@ -4,7 +4,7 @@ var google = require('googleapis');
 var google_key = require('../secrets/google_key.json');
 
 // configure a JWT auth client
-let jwtClient = new google.auth.JWT(
+let jwtClient = new google.google.auth.JWT(
     google_key.client_email,
     null,
     google_key.private_key,
